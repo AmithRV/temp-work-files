0cec4592a3577ec49ea810f34535b0f58f7f1535
AmithRV <amithrajv1999@gmail.com>
2022-04-19 23:07:16 +0530

    'form'

--- src/Components/Newform.js
+++ src/Components/Newform.js
@@ -17,12 +17,11 @@ function FormikFormDemo() {
     const [showMessage, setShowMessage] = useState(false);
     const [formData, setFormData] = useState({});
 
-   
+
     const formik = useFormik({
         initialValues: {
             name: '',
             email: '',
-            password: '',
             date: null,
             country: null,
             accept: false
@@ -41,9 +40,6 @@ function FormikFormDemo() {
                 errors.email = 'Invalid email address. E.g. example@email.com';
             }
 
-            if (!data.password) {
-                errors.password = 'Password is required.';
-            }
 
             if (!data.accept) {
                 errors.accept = 'You need to agree to the terms and conditions.';
@@ -52,6 +48,7 @@ function FormikFormDemo() {
             return errors;
         },
         onSubmit: (data) => {
+            console.log(data)
             setFormData(data);
             setShowMessage(true);
 
@@ -65,32 +62,9 @@ function FormikFormDemo() {
     };
 
     const dialogFooter = <div className="flex justify-content-center"><Button label="OK" className="p-button-text" autoFocus onClick={() => setShowMessage(false)} /></div>;
-    const passwordHeader = <h6>Pick a password</h6>;
-    const passwordFooter = (
-        <React.Fragment>
-            <Divider />
-            <p className="mt-2">Suggestions</p>
-            <ul className="pl-2 ml-2 mt-0" style={{ lineHeight: '1.5' }}>
-                <li>At least one lowercase</li>
-                <li>At least one uppercase</li>
-                <li>At least one numeric</li>
-                <li>Minimum 8 characters</li>
-            </ul>
-        </React.Fragment>
-    );
 
     return (
         <div className="form-demo">
-            {console.log('form data : ', formData)}
-            <Dialog visible={showMessage} onHide={() => setShowMessage(false)} position="top" footer={dialogFooter} showHeader={false} breakpoints={{ '960px': '80vw' }} style={{ width: '30vw' }}>
-                <div className="flex align-items-center flex-column pt-6 px-3">
-                    <i className="pi pi-check-circle" style={{ fontSize: '5rem', color: 'var(--green-500)' }}></i>
-                    <h5>Registration Successful!</h5>
-                    <p style={{ lineHeight: 1.5, textIndent: '1rem' }}>
-                        Your account is registered under name <b>{formData.name}</b> ; it'll be valid next 30 days without activation. Please check <b>{formData.email}</b> for activation instructions.
-                    </p>
-                </div>
-            </Dialog>
 
             <div className="flex justify-content-center">
                 <div className="card">
@@ -103,6 +77,7 @@ function FormikFormDemo() {
                             </span>
                             {getFormErrorMessage('name')}
                         </div>
+
                         <div className="field">
                             <span className="p-float-label p-input-icon-right">
                                 <i className="pi pi-envelope" />
@@ -111,26 +86,21 @@ function FormikFormDemo() {
                             </span>
                             {getFormErrorMessage('email')}
                         </div>
-                        <div className="field">
-                            <span className="p-float-label">
-                                <Password id="password" name="password" value={formik.values.password} onChange={formik.handleChange} toggleMask
-                                    className={classNames({ 'p-invalid': isFormFieldValid('password') })} header={passwordHeader} footer={passwordFooter} />
-                                <label htmlFor="password" className={classNames({ 'p-error': isFormFieldValid('password') })}>Password*</label>
-                            </span>
-                            {getFormErrorMessage('password')}
-                        </div>
+                        
                         <div className="field">
                             <span className="p-float-label">
                                 <Calendar id="date" name="date" value={formik.values.date} onChange={formik.handleChange} dateFormat="dd/mm/yy" mask="99/99/9999" showIcon />
                                 <label htmlFor="date">Birthday</label>
                             </span>
                         </div>
+                        
                         <div className="field">
                             <span className="p-float-label">
                                 <Dropdown id="country" name="country" value={formik.values.country} onChange={formik.handleChange} options={countries} optionLabel="name" />
                                 <label htmlFor="country">Country</label>
                             </span>
                         </div>
+                        
                         <div className="field-checkbox">
                             <Checkbox inputId="accept" name="accept" checked={formik.values.accept} onChange={formik.handleChange} className={classNames({ 'p-invalid': isFormFieldValid('accept') })} />
                             <label htmlFor="accept" className={classNames({ 'p-error': isFormFieldValid('accept') })}>I agree to the terms and conditions*</label>
